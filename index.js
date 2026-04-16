770013c3ddc695e7f22eaddc2a3302fc57b705ad
rajrai <rajrai913197@gmail.com>
2026-04-16 15:55:31 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-// write a program to take 2 num from user then print 2 num
+// function Pro() {
+//     let user = prompt(" what is your first name ");
+//     let lol = prompt(" what is your sec name ");
+//      alert(user+lol)
+// }
+// Pro()
+// function Add(){
+//     let user = parseInt(prompt(" what is your first num "));
+//    let lol = parseInt(prompt("second num"));
+//    alert(user+lol)
+// }
+// Add()
 
 
+// let  Age = prompt("enter your age")
+// if (Age > 18) {
+//     console.log("you acn vote");
+    
+// }else if(Age < 18){
+// console.log("you cant vote");
 
-// let a = parseInt(prompt("enter first number"));
-// let b = parseInt(prompt("enter second number"));
-// let swip = a;
-// a = b;
-// b = swip;
-// console.log("first number is " + a);
-// console.log("second number is " + b);
+// }
+ 
+// // 
+//  i = 1;
+//  for (let i = 1 ; i <= 10; i++) {
+//     console.log(i);
+    
+    
+//  }
